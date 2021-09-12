fd0beada7994fc0be5ec853fbb5b1e36310f6711
Anna Mayboroda <mayboroda1985@gmail.com>
2021-09-11 22:47:14 -0400

    added folders and files with homework

---
+++ Lecture_1/homework1.js
@@ -0,0 +1,165 @@
+//LESSON 1 HOMEWORK
+
+
+//1. Create the variable with the name => first and declare the value 'Hello'
+
+let first = 'Hello';
+
+
+//2. Create the variable with the name => second and declare the value 'World!'
+let second = 'World!';
+
+
+//3. Create the variable with the name => greeting and declare the value of the variable first and second
+
+let greeting = first + " " + second;
+
+//4. Print the result of greeting to console
+
+console.log(greeting);
+
+//the result in the end should be "Hello World!"
+
+
+
+//===================================
+
+//1. Create the variable with the name => numberOne, and declare the value to 10, Print the result to console
+let numberOne= 10 ;
+console.log(numberOne);
+
+
+//2. Create the variable with the name => numberTwo, and declare the value to 12, Print the result to console.
+
+let numberTwo = 12;
+console.log(numberTwo);
+
+//3. Create the variable with the name => sum(сумма), and declare the value of numberOne plus numberTwo, Print the result to console.
+
+let sum = numberOne + numberTwo;
+console.log(sum);
+
+//4. Create the variable with the name => subtract(отнять), and declare the result of  numberOne minus numberTwo (and reverse numberTwo minus numberOne), Print both results to console.
+let substract1 = numberOne - numberTwo;
+let substract2 = numberTwo - numberOne;
+console.log(substract1);
+console.log(substract2);
+
+//5.Do the same with multiplication and division.
+
+let multiply = numberOne * numberTwo;
+let devide = numberTwo / numberOne;
+console.log(multiply);
+console.log(devide);
+//===================================
+
+//1. Create the variable with the name => name, declare the value "John", Print the result to console.
+
+let name = "John";
+console.log(name);
+
+//2. Create the variable with the name => age, declare the value 41, Print the result to console.
+let age = 41;
+console.log(age);
+
+//3. Create the variable with the name gratings, declare the value of the sum of variables name & age, Print the result to console.
+let gratings = name + ' ' + age;
+console.log(gratings);
+console.log(line);
+
+//===================================
+
+
+//2.Print the result of string concatination
+// let name1 = "Hello";
+// let name2 = 'World';
+// let str = name1 + " " + name2;
+// console.log(str);
+
+//3.Print the result of multiplication
+let a = 4;
+let b = 2;
+let multiple = a * b;
+console.log(multiple);
+
+//===================================
+
+// let perimeter = 2 * (a + b);
+// let area = a * b;
+// console.log(perimeter);
+// console.log(area);
+
+//1. Crate a variable with name - price and assign the value 5;
+
+let price = 5;
+//2. Create a variable with name - amount and assign the value 10;
+let amount = 10;
+
+//3. Create a variable with name totalPrice an d Calculate the results;
+let totalPrice = price + amount;
+console.log(totalPrice);
+//4. Console log string 'The total price is {put total price here} for {put amount here} items'
+console.log(`The total price is ${totalPrice} for ${amount} items`);
+
+//1.
+
+let a = 10;
+let b = 5;
+let c ;
+c = a;
+a = b;
+b = c;
+console.log(c)
+
+//change the value to a = 5, b = 10;
+
+a = 5;
+b = 10;
+console.log(a);
+console.log(b);
+
+//1. Find the perimetr and area of the square. Console the result.
+let squareSide = 10;
+let perimeter = 4 * squareSide;
+let area;
+area = squareSide ** 2;
+console.log(perimeter);
+
+//2. Find the perimetr and area of the rectangle. Console the result.
+// let a = 10;
+// let b = 5;
+
+// https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/javascript
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+console.log(name);
+
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+
+function greet(name){
+    //your code here
+    let name = "Anna";
+    console.log(`Hello, ${name} how are you doing today?`)
+}
+
+// https://www.codewars.com/kata/5859d3325a8a0747180000c6/train/javascript
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
