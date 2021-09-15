c24a0a06441caf683a8dc9bbf4f1b56ebdcaf1f9
Anna Mayboroda <mayboroda1985@gmail.com>
2021-09-15 17:37:44 -0400

    new problems added , task 12

--- useful_info/charcode.js
+++ useful_info/charcode.js
@@ -1,3 +1,6 @@
 let char1 = "G";
 let char2 = "g";
 console.log(char1.charCodeAt() < char2.charCodeAt());
+
+to check unicode symbols weight go to:
+//UNICODE TABLE(DECIMAL) www.tamasoft.co.jp/en/general-info/unicode-decimal.html
