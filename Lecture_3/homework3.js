7fbacba0ac2b4cf4b97ed956eb8751d8cda8a691
Anna Mayboroda <mayboroda1985@gmail.com>
2021-09-16 16:23:31 -0400

    added new lecture and homework
    added new lecture and homework

---
+++ Lecture_3/homework3.js
@@ -0,0 +1,88 @@
+// LESSON 3 HOMEWORK
+
+// #1
+// fix the code so that the console displays: "undefined"
+
+// let a;
+// let b = 3;
+// console.log(a);
+
+
+// #2
+// Create the variable with the name name, and declare the value - any name of your choise;
+
+// Create the variable with the name age, and declare the any value from the range [1; 80];
+
+// Create the variables with the names: isChild; isTeenager; isAdult.
+// These variables should contain boolean values, the age must be taken
+// into account: child: [1; 10] ,  teenager: [11; 18] ,  adult: 19 and over.
+
+// print 3 phrases to the console, whether the person is a child,
+// or a teenager, or an adult, according this form: "Is Anna a child? false"
+
+
+// #3
+// Задайте переменную х;
+// Назначьте переменную typeX, равную типу данных х;
+// Выведите в консоль тип переменной typeX.
+
+
+// #4 Склад магазина получил 20 утюгов, 15 микроволновок, 12 кофварок и кастрюли.
+// Создайте переменные с подходящими названиями и переменную для общего
+// количества предметов, запишите в них соответствующие значения;
+
+// Выведите в консоль общее количество предметов.
+
+// Будьте осторожны с пропущенными в документах данными! :)
+
+
+// #5
+// Создайте переменные со значениями: null; undefined; "word"; ''; NaN;
+// (NaN + ''); ('a' - 7) .
+
+// Проверьте, являются ли они «не числом»  ->  isNaN();
+
+// Проверьте те же данные, равны ли они NaN ?
+
+
+// #6
+// fix the code using parentheses, expected result: false
+// console.log(true || undefined === null && 0 > 1 );
+
+
+// #7
+// fix the code using ! (NOT); expected result: null
+// console.log(undefined && null && true );
+
+
+// #8
+// Given three numbers: d = 1.1; e = -1.1; f = 0.
+// Use comparison operators to print to the console
+// if these numbers are not positive?
+
+
+// #9
+// let num1 = "5";
+// let num2 = " 3.4";
+// let num3 = " ";
+// let num4 = true;
+// найдите сумму переменных
+
+// Переведите переменные в number и снова найдите сумму
+
+
+// #10
+// --- baNaNa ---
+// используя только эти переменные, вывести в консоль слово "baNaNa"
+// let a = 'a';
+// let b = 'b';
+
+
+//https://www.codewars.com/kata/560f8d41cf6e1fe5c900002e/train/javascript
+//https://www.codewars.com/kata/56c24c58e0c0f741d4001aef/train/javascript
+//https://www.codewars.com/kata/55a4f9afeffe4231090000d6/train/javascript
+//https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+
+
+
+//
